9413daf152ef6dfb2e91076f1ac91786d17f1faf
nishi-2400 <a7w210872424bump@gmail.com>
2023-10-07 11:20:37 +1000

    List Editscreen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StyleSheet } from 'react-native';
 import MemoListScreen from './src/screens/MemoListScree';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
     // <MemoListScreen></MemoListScreen>
-    <MemoDetailScreen></MemoDetailScreen>
+    // <MemoDetailScreen></MemoDetailScreen>
+    <MemoEditScreen></MemoEditScreen>
   );
 }
